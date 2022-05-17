d5e71e500be302f0b8b26ff88d19588e4f0021d1
niklai99 <lai.nicolo99@gmail.com>
2022-05-17 16:00:10 +0200

    further exploration ideas

---
+++ eda/modules/plot_utils.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def plot_feature_distribution(
+    feature,
+    fig, 
+    sub_id = 111,
+    bins   = 20,
+    lw     = 3,
+    ecolor = "white",
+    color  = "#7eb0d5"
+):
+    
+    ax = fig.add_subplot(sub_id)
+    
+    ax.hist(
+        feature,
+        bins      = bins,
+        histtype  = "bar", 
+        linewidth = lw,
+        edgecolor = ecolor, 
+        facecolor = color, 
+        alpha     = 1, 
+    )
+    
+    return ax
+
+
+def plot_feature_correlation(
+    x,
+    y,
+    fig, 
+    sub_id = 111,
+    color  = "#7eb0d5",
+    size   = 20
+):
+
+    ax = fig.add_subplot(sub_id)
+    
+    ax.scatter(
+        x     = x,
+        y     = y,
+        color = color,
+        s     = size
+    )
+    
+    return ax
