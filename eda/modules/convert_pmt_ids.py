e4b2c47394001501a0990a34c60b3695925e8efa
c0pp1 <albertocoppi@outlook.it>
2022-05-30 16:51:35 +0200

    fixed fixing_shapes

--- eda/modules/convert_pmt_ids.py
+++ eda/modules/convert_pmt_ids.py
@@ -4,5 +4,5 @@ import pandas as pd
 def convert_pmt_ids(input_ids, conversion_ids):
     cd_ids = np.array(conversion_ids['CdID'])
     pmt_ids = np.array(conversion_ids['PMTID'])
-    indices = np.where(np.in1d(cd_ids, input_ids))[0]
-    return pmt_ids[indices]
+    mask = np.isin(cd_ids, input_ids)
+    return pmt_ids[mask]
